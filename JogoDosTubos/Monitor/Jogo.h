ca64609a6556086c3ffb3d57d11111ecf91f92e6
Jota379 <51248668+Jota379@users.noreply.github.com>
2022-06-19 17:10:59 +0100

    nova estrutura

--- JogoDosTubos/Monitor/Jogo.h
+++ JogoDosTubos/Monitor/Jogo.h
@@ -16,8 +16,13 @@
 typedef struct JOGODOSTUBOS {
 	TCHAR tab[20][20];
 	int altura, largura, tempo_agua;
-	int stop,ganhou;
+	int stop, ganhou;
 }JOGO;
+
+typedef struct COMUNICAÇÂO {
+	int posx, posy;
+	TCHAR c;
+}CLITOSERV;
 
 typedef struct {
 	JOGO jogo;
